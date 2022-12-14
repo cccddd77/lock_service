8e0fd9b0e413690fcba6597950d07c04aab332d8
chen de <dg20000106@gmail.com>
2022-12-14 12:59:09 +0800

    implement lock service and fake at-most-once

--- client/client.go
+++ client/client.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"log"
 	"time"
+	"math/rand"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -21,32 +22,41 @@ type request struct {
 	Operator string
 }
 
-var testDate = [7]request{
-	{1, "acquire"},
-	{2, "release"},
-	{3, "acquire"},
-	{4, "acquire"},
-	{5, "release"},
-	{6, "release"},
-	{7, "acquire"},
-}
-
 func sendReq(client pb.LockServiceClient) {
 	log.Print("Sending requests.")
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	for _, i := range testDate {
-		log.Print("Send: ", i.CliID, " ", i.Operator)
-		switch i.Operator {
+	lockedClient := int64(0)
+	for i := 0; i < 50; i++ {
+		req := request{CliID: rand.Int63n(7) + 1}
+		if lockedClient != req.CliID {
+			req.Operator = "acquire"
+		} else {
+			switch rand.Intn(2) {
+			case 0:
+				req.Operator = "acquire"
+			case 1:
+				req.Operator = "release"
+			}
+		}
+
+		log.Print("Sends: CliID ", req.CliID, " to ", req.Operator)
+		switch req.Operator {
 		case "acquire":
-			rsp, err := client.DoLock(ctx, &pb.Req{CliID: i.CliID, Operator: i.Operator})
+			rsp, err := client.DoLock(ctx, &pb.Req{CliID: req.CliID, Operator: req.Operator})
 			if err == nil {
 				log.Print("Receive: ", rsp.CliID, " ", rsp.Operator, " ", rsp.Msg)
+				if rsp.Msg == "success" {
+					lockedClient = rsp.CliID
+				}
 			}
 		case "release":
-			rsp, err := client.UnLock(ctx, &pb.Req{CliID: i.CliID, Operator: i.Operator})
+			rsp, err := client.UnLock(ctx, &pb.Req{CliID: req.CliID, Operator: req.Operator})
 			if err == nil {
 				log.Print("Receive: ", rsp.CliID, " ", rsp.Operator, " ", rsp.Msg)
+				if rsp.Msg == "success" {
+					lockedClient = 0
+				}
 			}
 		}
 	}
--- server/server.go
+++ server/server.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"sync"
 
 	"google.golang.org/grpc"
 
@@ -18,29 +19,95 @@ var (
 
 type lockServiceServer struct {
 	pb.UnimplementedLockServiceServer
+	lockedClient int64
+	clientReqCache map[int64]string
+	mutex sync.Mutex
+	changed bool
+}
+
+func (s *lockServiceServer) getlock(id int64) string {
+	var msg string
+	s.mutex.Lock()
+	if s.lockedClient == 0 {
+		s.lockedClient = id
+		s.changed = true;
+		msg = "success"
+	} else {
+		msg = "failure"
+		s.changed = false;
+	}
+	s.mutex.Unlock()
+	return msg
 }
 
 func (s *lockServiceServer) DoLock(ctx context.Context, req *pb.Req) (*pb.Rsp, error) {
 	var rsp pb.Rsp
 	rsp.CliID = req.CliID
 	rsp.Operator = req.Operator
-	rsp.Msg = "success"
-	log.Print("Client ID: ", rsp.CliID, ", ", "Operator: ", rsp.Operator, "\n")
+	tmp, ok := s.clientReqCache[rsp.CliID]
+	if ok {
+		if tmp == rsp.Operator {
+			if s.changed {
+				rsp.Msg = s.getlock(rsp.CliID)
+				s.clientReqCache[rsp.CliID] = rsp.Operator
+			} else {
+				rsp.Msg = "repeat"
+			}
+		} else {
+			rsp.Msg = s.getlock(rsp.CliID)
+			s.clientReqCache[rsp.CliID] = rsp.Operator
+		}
+	} else {
+		rsp.Msg = s.getlock(rsp.CliID)
+		s.clientReqCache[rsp.CliID] = rsp.Operator
+	}
+	log.Print("Client ID: ", rsp.CliID, ", ", "Operator: ", rsp.Operator, " ", rsp.Msg, "\n")
 	return &rsp, nil
+}
+
+func (s *lockServiceServer) droplock(id int64) string {
+	var msg string
+	s.mutex.Lock()
+	if s.lockedClient == id {
+		s.lockedClient = 0
+		s.changed = true
+		msg = "success"
+	} else {
+		s.changed = false
+		msg = "failure"
+	}
+	s.mutex.Unlock()
+	return msg
 }
 
 func (s *lockServiceServer) UnLock(ctx context.Context, req *pb.Req) (*pb.Rsp, error) {
 	var rsp pb.Rsp
 	rsp.CliID = req.CliID
 	rsp.Operator = req.Operator
-	rsp.Msg = "success"
-	log.Print("Client ID: ", rsp.CliID, ", ", "Operator: ", rsp.Operator, "\n")
+	tmp, ok := s.clientReqCache[rsp.CliID]
+	if ok {
+		if tmp == rsp.Operator {
+			if s.changed {
+				rsp.Msg = s.droplock(rsp.CliID)
+				s.clientReqCache[rsp.CliID] = rsp.Operator
+			} else {
+				rsp.Msg = "repeat"
+			}
+		} else {
+			rsp.Msg = s.droplock(rsp.CliID)
+			s.clientReqCache[rsp.CliID] = rsp.Operator
+		}
+	} else {
+		rsp.Msg = s.droplock(rsp.CliID)
+		s.clientReqCache[rsp.CliID] = rsp.Operator
+	}
+	log.Print("Client ID: ", rsp.CliID, ", ", "Operator: ", rsp.Operator, " ", rsp.Msg, "\n")
 	return &rsp, nil
 }
 
 
 func newServer() *lockServiceServer {
-	s := &lockServiceServer{}
+	s := &lockServiceServer{clientReqCache: make(map[int64]string)}
 	return s
 }
 
@@ -49,6 +116,8 @@ func main() {
 	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
+	} else {
+		log.Print("Listening at localhost: ", *port)
 	}
 	grpcServer := grpc.NewServer()
 	pb.RegisterLockServiceServer(grpcServer, newServer())
